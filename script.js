2bdb164e451eb1f70ae60f88590cf990cd612e06
Ananthakrishnan U S <ananthakrishnan1213@gmail.com>
2022-07-11 16:07:15 +0530

    commit before check

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+
+function validate(mycallback){
+    let user=document.getElementById("user");
+    let password=document.getElementById("password");
+
+if(user.value==='admin' && password.value==12345){
+    mycallback();
+
+}
+else{
+    alert("Wrong credentials");
+}
+
+
+}
+
+
+
 function form_input(){
 
-    x=document.getElementById("body");
+    var x=document.getElementById("body");
     x.innerHTML=`<nav class="navbar navbar-light bg-light justify-content-between">
     <a class="navbar-brand" style="margin-left:15px;">To Do</a>
     <form class="form-inline">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="margin-right:20px;">Log Out</button>
+      <button id="logout" class="btn btn-outline-success my-2 my-sm-0" type="submit" style="margin-right:20px;" onclick="logout()">Log Out</button>
     </form>
   </nav>
   
@@ -21,20 +39,16 @@ function form_input(){
    
   </tbody>
 </table>`
-
-
-
-
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function(){
-    if(this.readyState==4 && this.status==200){
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+if(this.readyState==4 && this.status==200){
     var response = JSON.parse(this.responseText);
 
     for(let i=0;i<response.length;i++){
      document.getElementById("tbody").innerHTML+=` <tr>
      <td style="width:200px;">${response[i].id}</td>
      <td>${response[i].title}</td>
-     <td style="width:30px;";><input type="checkbox"</td>
+     <td style="width:30px;";><input type="checkbox" class="chk"></td>
      </tr>`
       }
 
@@ -47,3 +61,38 @@ function form_input(){
 
 
 }
+
+function logout(){
+    let out = document.getElementById("logout");
+     x.innerHTML=`<div class="row" style="height: 100px;margin-top:100px">
+     <h1 style="text-align: center;font-size: 70px;">TO DO</h1>
+     </div>
+     <div class="row">
+       <div class="col"></div>
+       <div class="col">
+         <h3 style="text-align: center;margin-bottom: 40px;">Log in</h3>
+         <form action="#" >
+           <label for="user" style="width: 100%;text-align: center;"><h5>Username</h5></label>
+           <br>
+           <input id="user" type="text" name="user" placeholder="John Doe" style="width: 100%;text-align: center;border-radius: 10px;">
+            <br>
+            <br>
+           <label for="password" style="width: 100%;text-align: center;"><h5>Password</h5></label>
+           <br>
+           <input id="password" type="password" name="password" placeholder="**********" style="width: 100%;text-align: center;border-radius: 10px;">
+           <br>
+           <input id="submit" type="submit" style="width: 20%;text-align: center;border-radius: 10px;margin-top: 20px;margin-left: 180px;" onclick="validate(form_input)">
+   
+   
+   
+   
+         </form>
+   
+         <p style="margin-top: 25px; text-align:center;color: red;">Do not have an account?</p>
+         <h6 style="text-align: center;"><a href="#">Sign Up</a></h6>
+       </div>
+       <div class="col"></div>
+     </div>
+     <script src="script.js"></script>`
+}
+
